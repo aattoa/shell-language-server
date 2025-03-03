f265d575b6c507507534414845b776ea868ba6af
aattoa <aattoa@proton.me>
2025-03-03 21:09:35 +0200

    Remove trailing newline from shfmt output

--- src/external/shfmt.rs
+++ src/external/shfmt.rs
@@ -1,43 +1,52 @@
 use crate::lsp;
 use crate::shell::Shell;
 
+fn dialect_flag(shell: Shell) -> &'static str {
+    // Treat unsupported shells as POSIX, since shfmt can still format with decent accuracy.
+    // -ln is short for --language-dialect, and -p is short for -ln=posix.
+    match shell {
+        Shell::Bash => "-ln=bash",
+        Shell::Ksh => "-ln=mksh",
+        _ => "-p",
+    }
+}
+
+fn remove_trailing_newline(mut string: String) -> String {
+    match string.pop() {
+        None | Some('\n') => {}
+        Some(char) => {
+            eprintln!("[debug] shfmt output did not end with a newline");
+            string.push(char);
+        }
+    }
+    string
+}
+
 pub fn format(
     shell: Shell,
     options: lsp::FormattingOptions,
     shfmt_path: &str,
     document_text: &str,
 ) -> std::io::Result<String> {
-    use std::io::{Read, Write};
     use std::process::{Command, Stdio};
 
     // shfmt uses tabs if 0 is given as the indent width.
     let indent = if options.use_spaces { options.tab_size } else { 0 };
 
-    // Treat unsupported shells as POSIX, since shfmt can still format with decent accuracy.
-    let dialect = match shell {
-        Shell::Bash => "bash",
-        Shell::Ksh => "mksh",
-        _ => "posix",
-    };
-
     let mut child = Command::new(shfmt_path)
         .arg(format!("--indent={indent}"))
-        .arg(format!("--language-dialect={dialect}"))
+        .arg(dialect_flag(shell))
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()?;
 
-    let mut string = String::new();
-    child.stdin.take().unwrap().write_all(document_text.as_bytes())?;
-    child.stdout.take().unwrap().read_to_string(&mut string)?;
+    std::io::Write::write_all(&mut child.stdin.take().unwrap(), document_text.as_bytes())?;
 
     if child.wait()?.success() {
-        Ok(string)
+        std::io::read_to_string(child.stdout.take().unwrap()).map(remove_trailing_newline)
     }
     else {
-        let mut error = String::new();
-        child.stderr.take().unwrap().read_to_string(&mut error)?;
-        Err(std::io::Error::other(error))
+        Err(std::io::Error::other(std::io::read_to_string(child.stderr.take().unwrap())?))
     }
 }
